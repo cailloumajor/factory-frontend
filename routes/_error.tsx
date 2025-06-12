319939fc23b65baf74ccacc11beddd9e8ff16cd8
Arnaud Rocher <arnaud.roche3@gmail.com>
2025-06-12 08:58:39 +0000

    Add error page

---
+++ routes/_error.tsx
@@ -0,0 +1,31 @@
+import { HttpError } from "fresh"
+
+import { getTranslateFn } from "../i18n.ts"
+import { define } from "../utils.ts"
+
+export default define.page(function ErrorPage({ state, error, req }) {
+  // FIXME: https://github.com/denoland/fresh/issues/2843
+  const t = state.t ?? getTranslateFn(req)
+
+  const status = error instanceof HttpError ? error.status : 500
+
+  return (
+    <main class="fixed top-0 bottom-0 left-0 right-0 flex flex-col justify-center text-center bg-blue-400 text-white">
+      <h1 class="text-9xl/normal">{status}</h1>
+      <h2 class="text-6xl opacity-40">
+        {
+          // @ts-ignore: template literal cannot be typed
+          t([`error.${status}`, "error.other"])
+        }
+      </h2>
+      {status === 400 && (
+        <a
+          class="cursor-pointer bg-white text-blue-400 font-medium mx-auto mt-8 px-4 py-2 rounded-sm"
+          href="/"
+        >
+          {t("goHome")}
+        </a>
+      )}
+    </main>
+  )
+})
