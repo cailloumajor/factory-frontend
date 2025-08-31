1be618487a5d214ce1c0d02251450aeb198fcea9
Arnaud Rocher <arnaud.roche3@gmail.com>
2025-08-31 22:07:03 +0000

    Restore dev API with Vite

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { App, staticFiles } from "fresh"
 
+import { devRoutes } from "./routes-dev/index.ts"
 import { i18n } from "./utils/i18n.ts"
 import type { State } from "./utils/state.ts"
 import { appConfig } from "./utils/config.ts"
@@ -9,3 +10,7 @@ export const app = new App<State>()
   .use(staticFiles())
   .use(await i18n())
   .fsRoutes()
+
+if (import.meta.env.MODE === "development") {
+  devRoutes(app)
+}
--- routes-dev/index.ts
+++ routes-dev/index.ts
@@ -4,13 +4,39 @@ import type { App } from "fresh"
 import { getAppConfig } from "../utils/config.ts"
 import type { State } from "../utils/state.ts"
 
-import timelineData from "./timeline_data.bin" with { type: "bytes" }
+import timelineDataPath from "./timeline_data.bin?url"
 
 export function devRoutes(app: App<State>) {
-  const appConfig = getAppConfig()
+  const requestCount = {
+    config: 0,
+    timeline: 0,
+  }
 
-  app.get(
-    posix.join(appConfig.apiBaseUrl.computeApi, "timeline", ":id"),
-    (_ctx) => new Response(timelineData),
-  )
+  const { apiBaseUrl } = getAppConfig()
+
+  const configUrl = posix.join(apiBaseUrl.configApi, "line_dashboard", ":id")
+  const timelineUrl = posix.join(apiBaseUrl.computeApi, "timeline", ":id")
+
+  app.get(configUrl, ({ params }) => {
+    requestCount.config += 1
+    if (requestCount.config % 2 === 0) {
+      return Response.json(false)
+    }
+    return Response.json({
+      title: `dev title (${params.id})`,
+      targetCycleTime: 60,
+      targetEfficiency: 1,
+    })
+  })
+
+  app.get(timelineUrl, async ({ url }) => {
+    requestCount.timeline += 1
+    if (requestCount.timeline % 2 === 0) {
+      return new Response(null, { status: 403 })
+    }
+    const timelineDataUrl = new URL(url)
+    timelineDataUrl.pathname = timelineDataPath
+    const timelineData = await fetch(timelineDataUrl).then((resp) => resp.arrayBuffer())
+    return new Response(timelineData)
+  })
 }
