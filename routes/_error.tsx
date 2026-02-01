12f0a2202d0e7b98811c487a2e14e2bfc7daaf2b
Arnaud Rocher <arnaud.roche3@gmail.com>
2026-02-01 15:33:49 +0000

    Only render an error page on HTTP error

--- routes/_error.tsx
+++ routes/_error.tsx
@@ -1,40 +1,28 @@
-import { HttpError } from "fresh"
+import { HttpError, page } from "fresh"
 
 import { define } from "@/utils/state.ts"
 
-interface ServerCodePageProps {
-  code?: number
-  description: string
-  goHomeText: string
-}
+export const handler = define.handlers({
+  GET(ctx) {
+    if (ctx.error instanceof HttpError) {
+      return page({ errorCode: ctx.error.status })
+    }
 
-function ServerCodePage(props: ServerCodePageProps) {
+    // deno-lint-ignore no-console -- console is used by purpose
+    console.error(ctx.error)
+
+    return ctx.next()
+  },
+})
+
+export default define.page<typeof handler>((ctx) => {
   return (
     <main class="fixed top-0 bottom-0 left-0 right-0 flex flex-col justify-center text-center">
-      {props.code && <h1 class="text-9xl/normal text-error">{props.code}</h1>}
-      <h2 class="text-6xl opacity-40">{props.description}</h2>
-      <a class="btn mt-8 mx-auto" href="/">{props.goHomeText}</a>
+      {ctx.data.errorCode && <h1 class="text-9xl/normal text-error">{ctx.data.errorCode}</h1>}
+      <h2 class="text-6xl opacity-40">
+        {ctx.state.t(($) => $.httpError[ctx.data.errorCode === 404 ? "404" : "unspecific"])}
+      </h2>
+      <a class="btn mt-8 mx-auto" href="/">{ctx.state.t(($) => $.goHome)}</a>
     </main>
   )
-}
-
-export default define.page(({ state, error }) => {
-  const goHomeText = state.t(($) => $.goHome)
-
-  if (error instanceof HttpError) {
-    const description = state.t(($) => $.httpError[error.status === 404 ? "404" : "unspecific"])
-    return ServerCodePage({
-      code: error.status,
-      description,
-      goHomeText,
-    })
-  }
-
-  // deno-lint-ignore no-console -- console is used by purpose
-  console.error(error)
-
-  return ServerCodePage({
-    description: state.t(($) => $.httpError.unspecific),
-    goHomeText,
-  })
 })
