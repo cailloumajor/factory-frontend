a0604eaa3de1b6f55d2fb218a1bdff66e252e9a6
Arnaud Rocher <arnaud.roche3@gmail.com>
2026-05-03 12:37:35 +0200

    Upgrade Fresh

--- routes-dev/mod.ts
+++ routes-dev/mod.ts
@@ -1,5 +1,4 @@
 import { decodeBase64 } from "@std/encoding"
-import { serveDir } from "@std/http"
 import type { App } from "fresh"
 
 import type { State } from "@/utils/state.ts"
@@ -8,11 +7,6 @@ import timelineData from "./timeline_data.bin?url&inline"
 import { faker } from "@faker-js/faker"
 
 export function devRoutes(app: App<State>) {
-  // TODO: remove this hack if https://github.com/denoland/fresh/issues/3424 gets fixed.
-  app.get("/node_modules/.deno/@fontsource-variable*", ({ req }) => {
-    return serveDir(req, { showDotfiles: true, quiet: true })
-  })
-
   app.get("/dev-config-api/config/line_dashboard/:id", ({ json, params }) => {
     if (faker.datatype.boolean(0.4)) {
       return json(false)
